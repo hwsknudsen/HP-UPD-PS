c19b4f3c68e286b7f73be50875c1498db592ec84
hwsknudsen <hwsknudsen@users.noreply.github.com>
2023-11-28 10:55:00 -0400

    Add files via upload

---
+++ Detect Script.ps1
@@ -0,0 +1,8 @@
+﻿$File = "C:\Windows\system32\DriverStore\FileRepository\hpcu270u.inf_amd64_3e20dbae029ad04a"
+if (Test-Path $File) {
+    write-output "HP UPD Detected"
+    exit 0
+}
+else {
+    exit 1
+}
---
+++ HPUPDInstaller.ps1
@@ -0,0 +1,21 @@
+﻿$source = "https://ftp.hp.com/pub/softlib/software13/printers/UPD/"
+$version = "upd-pcl6-x64-7.1.0.25570"
+$destination = "c:\TempHPUPD\"
+$drivertestpath = "C:\Windows\system32\DriverStore\FileRepository\hpcu270u.inf_amd64_3e20dbae029ad04a"
+
+#Check if software is installed. If installed terminate script
+if ((Test-Path "c:\TempHPUPD\$version") -Or (Test-Path $drivertestpath)){
+write-host "Software already installed" 
+Exit
+}
+
+#install software
+mkdir $destination
+Invoke-WebRequest ($source+$version+".exe") -OutFile ($destination+$version+".zip")
+Expand-Archive ($destination+$version+".zip") -DestinationPath ($destination+$version)
+
+#Start-Process -FilePath ($destination+$version+"\Install.exe") -ArgumentList "/infremove /q /h" -Wait
+Start-Process -FilePath ($destination+$version+"\Install.exe") -ArgumentList "/infstage /q /h" -Wait
+
+#Delete installer
+Remove-Item -recurse $destination
